4796aca898d6bb412228c6309a224dec8d1d1dce
GabrielCavalcanteVelasquez <gabrielcrb5@gmail.com>
2025-12-10 17:34:33 -0300

    Questao 2 feita

---
+++ Q2_NASM/run_q2.sh
@@ -0,0 +1,5 @@
+1 - Use de preferencia um teminal Linux(Ubuntu/Fedora) para rodar o script (OBS: Caso esteja usando Windows, utilize o WSL)
+2 - Navegue ate o diretorio lm-assembly 
+3 - Para compilar o codigo digite em seu temrinal "make q2" (lembre-se de tirar as "  " , o que vale eh somente o que tem no meio)
+4 - A mensagem "Este trabalho foi realizado por Gabriel Velasquez e Thaynara Nascimento!" deve aparecer no terminal
+ 
